c65c5af6ebb3413b9fa023eb7da0f760f079437a
pedrosantoswav <pedrohs2016.clavedesol@gmail.com>
2026-04-15 10:20:51 -0300

    update-0.3.0

--- main/main.c
+++ main/main.c
@@ -4,7 +4,7 @@
  * Sistemas Embarcados
  * 
  * Autor: Pedro Henrique Silva dos Santos
- * Data: 10/04/2026
+ * Data: 14/04/2026
  * Versão: 0.3.0
  *
  * Descrição:
@@ -13,23 +13,23 @@
  * =========================================================
  */
 
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-#include <inttypes.h>
+#include <stdio.h>              // Biblioteca padrão de entrada/saída
+#include <string.h>             // Suporte às Strings
+#include <stdlib.h>             // Alocacação de Memória
+#include <inttypes.h>           // Definições de tipos inteiros com tamanho fixo
 
-#include "sdkconfig.h"
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
-#include "freertos/queue.h"
+#include "sdkconfig.h"          // Configurações do projeto ESP-IDF
+#include "freertos/FreeRTOS.h"  // Kernel do FreeRTOS
+#include "freertos/task.h"      // Manipulação de tarefas
+#include "freertos/queue.h"     // Filas
 #include "driver/gpio.h"
-
-#include "esp_chip_info.h"
-#include "esp_flash.h"
-#include "esp_system.h"
-#include "esp_log.h"
-#include "esp_idf_version.h"
 #include "driver/gptimer.h"
+
+#include "esp_chip_info.h"      // Informações sobre o chip ESP
+#include "esp_flash.h"          // Funções relacionadas à memória flash
+#include "esp_system.h"         // Funções gerais do sistema
+#include "esp_log.h"            // Sistema de logging (ESP_LOGI, ESP_LOGE, etc.)
+#include "esp_idf_version.h"    // Versão do ESP-IDF
 
 // =================== DEFINIÇÕES ===================
 
@@ -37,6 +37,7 @@
 #define B0 21
 #define B1 22
 #define B2 23
+
 #define GPIO_INPUT_PIN_SEL ((1ULL<<B0) | (1ULL<<B1) | (1ULL<<B2))
 
 // LED
@@ -52,7 +53,7 @@ static const char *TAG_TIMER = "TIMER_RT";
 static QueueHandle_t gpio_evt_queue = NULL;
 static QueueHandle_t timer_evt_queue = NULL;
 
-// =================== TIPOS ===================
+// ================== TIPOS ==================
 
 typedef struct {
     uint64_t count;
@@ -67,6 +68,8 @@ typedef struct {
 
 // =================== ISR ===================
 
+// Avalia se uma tarefa de maior prioridade foi desbloqueada e, se sim, solicita a troca de contexto imediata.
+
 static void IRAM_ATTR gpio_isr_handler(void* arg)
 {
     uint32_t gpio_num = (uint32_t) arg;
@@ -75,7 +78,9 @@ static void IRAM_ATTR gpio_isr_handler(void* arg)
     xQueueSendFromISR(gpio_evt_queue, &gpio_num, &xHigherPriorityTaskWoken);
 
     if (xHigherPriorityTaskWoken)
+    {
         portYIELD_FROM_ISR();
+    }
 }
 
 static bool IRAM_ATTR timer_callback(
@@ -103,7 +108,7 @@ static bool IRAM_ATTR timer_callback(
     return (high_task_awoken == pdTRUE);
 }
 
-// =================== TASKs ===================
+// =================== TASK ===================
 
 // LED
 static void led_control(void* arg)
@@ -119,22 +124,33 @@ static void led_control(void* arg)
 
             ESP_LOGI(TAG_GPIO, "GPIO[%" PRIu32 "] pressionado | Nivel: %d", io_num, level);
 
+            // ===== CONTROLE DO LED =====
+
             if (io_num == B0)
             {
                 gpio_set_level(LED, 1);
                 led_state = 1;
+                ESP_LOGI(TAG_GPIO, "LED LIGADO (Botao 0)");
             }
             else if (io_num == B1)
             {
                 gpio_set_level(LED, 0);
                 led_state = 0;
+                ESP_LOGI(TAG_GPIO, "LED DESLIGADO (Botao 1)");
             }
             else if (io_num == B2)
             {
                 led_state = !led_state;
                 gpio_set_level(LED, led_state);
 
-                ESP_LOGW(TAG_GPIO, "Botao 2 sem debounce");
+                ESP_LOGW(TAG_GPIO,
+                "Atenção!!! Lógica do Botão 2 não possui debounce, sujeito a múltiplos acionamentos");
+
+                if(led_state == 1)
+                    ESP_LOGI(TAG_GPIO, "LED LIGADO (Botão 2)");
+                
+                else
+                    ESP_LOGI(TAG_GPIO, "LED DESLIGADO (Botão 2)");
             }
         }
     }
@@ -219,16 +235,31 @@ void timer_task(void *arg)
 
 void app_main(void)
 {
+    // =================== INFO DO SISTEMA ===================
+
     ESP_LOGI(TAG_SYS, "Iniciando programa...");
 
-    // ===== INFO =====
     esp_chip_info_t chip_info;
     uint32_t flash_size;
 
     esp_chip_info(&chip_info);
 
     ESP_LOGI(TAG_SYS, "Chip: %s", CONFIG_IDF_TARGET);
+
+    unsigned major = chip_info.revision / 100;
+    unsigned minor = chip_info.revision % 100;
+
+    ESP_LOGI(TAG_SYS, "Revisao: v%d.%d", major, minor);
     ESP_LOGI(TAG_SYS, "Nucleos: %d", chip_info.cores);
+
+    ESP_LOGI(TAG_SYS, "WiFi: %s",
+        (chip_info.features & CHIP_FEATURE_WIFI_BGN) ? "SIM" : "NAO");
+
+    ESP_LOGI(TAG_SYS, "Bluetooth: %s",
+        (chip_info.features & CHIP_FEATURE_BT) ? "SIM" : "NAO");
+
+    ESP_LOGI(TAG_SYS, "BLE: %s",
+        (chip_info.features & CHIP_FEATURE_BLE) ? "SIM" : "NAO");
 
     if (esp_flash_get_size(NULL, &flash_size) == ESP_OK)
     {
@@ -236,36 +267,51 @@ void app_main(void)
                  flash_size / (1024 * 1024));
     }
 
+    ESP_LOGI(TAG_SYS, "Heap minimo: %" PRIu32 " bytes",
+             esp_get_minimum_free_heap_size());
+
     ESP_LOGI(TAG_SYS, "ESP-IDF: %s", esp_get_idf_version());
 
-    // ===== GPIO =====
+    // =================== CONFIGURA GPIO ===================
+
     gpio_config_t io_conf;
 
+    // BOTÕES
     io_conf.mode = GPIO_MODE_INPUT;
     io_conf.pin_bit_mask = GPIO_INPUT_PIN_SEL;
     io_conf.pull_up_en = GPIO_PULLUP_ENABLE;
+    io_conf.pull_down_en = GPIO_PULLDOWN_DISABLE;
     io_conf.intr_type = GPIO_INTR_NEGEDGE;
     gpio_config(&io_conf);
 
+    // LED
     io_conf.mode = GPIO_MODE_OUTPUT;
     io_conf.pin_bit_mask = GPIO_OUTPUT_PIN_SEL;
+    io_conf.pull_up_en = GPIO_PULLUP_DISABLE;
+    io_conf.pull_down_en = GPIO_PULLDOWN_DISABLE;
     io_conf.intr_type = GPIO_INTR_DISABLE;
     gpio_config(&io_conf);
 
-    // ===== FILAS =====
+    // =================== FILA ===================
+
     gpio_evt_queue = xQueueCreate(10, sizeof(uint32_t));
     timer_evt_queue = xQueueCreate(10, sizeof(timer_event_t));
 
-    // ===== TASKS =====
+    // =================== TASK ===================
+
+    // Criação da tarefa de controle do LED
     xTaskCreate(led_control, "led_control", 2048, NULL, 10, NULL);
     xTaskCreate(timer_task, "timer_task", 4096, NULL, 5, NULL);
 
-    // ===== ISR =====
+    // =================== ISR ===================
+
+    // Inicialização do serviço de interrupção
     gpio_install_isr_service(0);
 
+    // Associação dos pinos à interrupção
     gpio_isr_handler_add(B0, gpio_isr_handler, (void*) B0);
     gpio_isr_handler_add(B1, gpio_isr_handler, (void*) B1);
     gpio_isr_handler_add(B2, gpio_isr_handler, (void*) B2);
 
-    ESP_LOGI(TAG_GPIO, "Sistema pronto.");
+    ESP_LOGI(TAG_GPIO, "Sistema pronto. Aguardando entrada...");
 }
