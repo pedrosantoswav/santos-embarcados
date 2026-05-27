e20407d40c1cbd4837c31fbbb324a2c66eadf81b
pedrosantoswav <pedrohs2016.clavedesol@gmail.com>
2026-05-27 11:25:21 -0300

    0.6.0

--- main/main.c
+++ main/main.c
@@ -1,11 +1,11 @@
 /*
  * =========================================================
- * Projeto 5
+ * Projeto 6
  * Sistemas Embarcados
  * 
  * Autor: Pedro Henrique Silva dos Santos
- * Data: 13/05/2026
- * Versão: 0.5.1
+ * Data: 27/05/2026
+ * Versão: 0.6.0
  *
  * Features:
  * - Informações detalhadas do sistema
@@ -44,6 +44,142 @@
 #include "esp_adc/adc_cali.h"       // Calibração ADC
 #include "esp_adc/adc_cali_scheme.h"// Implementação de calibração (curve e line fitting)
 
+//Bibliotecas do exemplo I2C_OLED
+
+#include <unistd.h>
+#include <sys/lock.h>
+#include <sys/param.h>
+#include "esp_timer.h"
+#include "esp_lcd_panel_io.h"
+#include "esp_lcd_panel_ops.h"
+#include "esp_err.h"
+#include "driver/i2c_master.h"
+#include "lvgl.h"
+
+#include "esp_lcd_panel_vendor.h"
+
+// ================== DISPLAY ===================
+
+static const char *TAG = "example";
+
+#define I2C_BUS_PORT  0
+
+// Display Spec
+
+#define EXAMPLE_LCD_PIXEL_CLOCK_HZ    (400 * 1000)
+#define EXAMPLE_PIN_NUM_SDA           19
+#define EXAMPLE_PIN_NUM_SCL           18
+#define EXAMPLE_PIN_NUM_RST           -1
+#define EXAMPLE_I2C_HW_ADDR           0x3C
+
+// The pixel number in horizontal and vertical
+
+#define EXAMPLE_LCD_H_RES              128
+#define EXAMPLE_LCD_V_RES              CONFIG_EXAMPLE_SSD1306_HEIGHT
+
+// Bit number used to represent command and parameter
+#define EXAMPLE_LCD_CMD_BITS           8
+#define EXAMPLE_LCD_PARAM_BITS         8
+
+#define EXAMPLE_LVGL_TICK_PERIOD_MS    5
+#define EXAMPLE_LVGL_TASK_STACK_SIZE   (4 * 1024)
+#define EXAMPLE_LVGL_TASK_PRIORITY     2
+#define EXAMPLE_LVGL_PALETTE_SIZE      8
+#define EXAMPLE_LVGL_TASK_MAX_DELAY_MS 500
+#define EXAMPLE_LVGL_TASK_MIN_DELAY_MS 1000 / CONFIG_FREERTOS_HZ
+
+// To use LV_COLOR_FORMAT_I1, we need an extra buffer to hold the converted data
+static uint8_t oled_buffer[EXAMPLE_LCD_H_RES * EXAMPLE_LCD_V_RES / 8];
+// LVGL library is not thread-safe, this example will call LVGL APIs from different tasks, so use a mutex to protect it
+static _lock_t lvgl_api_lock;
+
+void example_lvgl_demo_ui(lv_display_t *disp)
+{
+    lv_obj_t *screen = lv_display_get_screen_active(disp);
+
+    lv_obj_t *label_title = lv_label_create(screen);
+    lv_obj_t *label = lv_label_create(screen);
+    lv_obj_t *label_time = lv_label_create(screen);
+
+    lv_label_set_text(label_title, "P6 - Pedro Santos");
+    lv_obj_align(label_title, LV_ALIGN_TOP_MID, 0, 0);
+
+    lv_label_set_long_mode(label, LV_LABEL_LONG_SCROLL_CIRCULAR);
+    lv_label_set_text(label, "Palavras Aleatorias");
+    lv_obj_set_width(label, lv_display_get_horizontal_resolution(disp));
+    lv_obj_align(label, LV_ALIGN_CENTER, 0, 0);
+
+    lv_label_set_text(label_time, "hh:mm:ss");
+    lv_obj_align(label_time, LV_ALIGN_BOTTOM_MID, 0, 0);
+}
+
+static bool example_notify_lvgl_flush_ready(esp_lcd_panel_io_handle_t io_panel, esp_lcd_panel_io_event_data_t *edata, void *user_ctx)
+{
+    lv_display_t *disp = (lv_display_t *)user_ctx;
+    lv_display_flush_ready(disp);
+    return false;
+}
+
+static void example_lvgl_flush_cb(lv_display_t *disp, const lv_area_t *area, uint8_t *px_map)
+{
+    esp_lcd_panel_handle_t panel_handle = lv_display_get_user_data(disp);
+
+    // This is necessary because LVGL reserves 2 x 4 bytes in the buffer, as these are assumed to be used as a palette. Skip the palette here
+    // More information about the monochrome, please refer to https://docs.lvgl.io/9.2/porting/display.html#monochrome-displays
+    px_map += EXAMPLE_LVGL_PALETTE_SIZE;
+
+    uint16_t hor_res = lv_display_get_physical_horizontal_resolution(disp);
+    int x1 = area->x1;
+    int x2 = area->x2;
+    int y1 = area->y1;
+    int y2 = area->y2;
+
+    for (int y = y1; y <= y2; y++) {
+        for (int x = x1; x <= x2; x++) {
+            /* The order of bits is MSB first
+                        MSB           LSB
+               bits      7 6 5 4 3 2 1 0
+               pixels    0 1 2 3 4 5 6 7
+                        Left         Right
+            */
+            bool chroma_color = (px_map[(hor_res >> 3) * y  + (x >> 3)] & 1 << (7 - x % 8));
+
+            /* Write to the buffer as required for the display.
+            * It writes only 1-bit for monochrome displays mapped vertically.*/
+            uint8_t *buf = oled_buffer + hor_res * (y >> 3) + (x);
+            if (chroma_color) {
+                (*buf) &= ~(1 << (y % 8));
+            } else {
+                (*buf) |= (1 << (y % 8));
+            }
+        }
+    }
+    // pass the draw buffer to the driver
+    esp_lcd_panel_draw_bitmap(panel_handle, x1, y1, x2 + 1, y2 + 1, oled_buffer);
+}
+
+static void example_increase_lvgl_tick(void *arg)
+{
+    /* Tell LVGL how many milliseconds has elapsed */
+    lv_tick_inc(EXAMPLE_LVGL_TICK_PERIOD_MS);
+}
+
+static void example_lvgl_port_task(void *arg)
+{
+    ESP_LOGI(TAG, "Starting LVGL task");
+    uint32_t time_till_next_ms = 0;
+    while (1) {
+        _lock_acquire(&lvgl_api_lock);
+        time_till_next_ms = lv_timer_handler();
+        _lock_release(&lvgl_api_lock);
+        // in case of triggering a task watch dog time out
+        time_till_next_ms = MAX(time_till_next_ms, EXAMPLE_LVGL_TASK_MIN_DELAY_MS);
+        // in case of lvgl display not ready yet
+        time_till_next_ms = MIN(time_till_next_ms, EXAMPLE_LVGL_TASK_MAX_DELAY_MS);
+        usleep(1000 * time_till_next_ms);
+    }
+}
+
 // ================= DEFINIÇÕES =================
 
 #define B0 21
@@ -67,6 +203,7 @@ static const char *TAG_GPIO  = "GPIO";
 static const char *TAG_TIMER = "TIMER";
 static const char *TAG_PWM   = "PWM";
 static const char *TAG_ADC   = "ADC";
+static const char *TAG_LCD   = "LCD";
 
 // ================= FILAS =================
 
@@ -74,6 +211,7 @@ static QueueHandle_t gpio_evt_queue = NULL;
 static QueueHandle_t timer_evt_queue = NULL;
 static QueueHandle_t pwm_evt_queue = NULL;
 static QueueHandle_t adc_queue = NULL;
+static QueueHandle_t lcd_queue = NULL;
 
 // ================= SEMÁFOROS =================
 
@@ -102,6 +240,11 @@ typedef struct {
     int raw;
     int voltage;
 } adc_data_t;
+
+typedef struct {
+    relogio_t relogio;
+    int voltage;
+} lcd_data_t;
 
 // ================= ISR =================
 
@@ -402,8 +545,17 @@ void timer_task(void *arg)
                 }
 
                 ESP_LOGI(TAG_TIMER, "Hora: %02lu:%02lu:%02lu",
-                         relogio.hora, relogio.minuto, relogio.segundo);
+                    relogio.hora, relogio.minuto, relogio.segundo);
 
+                char str_hora[9]; // "HH:MM:SS" + '\0'
+
+                snprintf(str_hora,
+                    sizeof(str_hora),
+                    "%02lu:%02lu:%02lu",
+                    relogio.hora,
+                    relogio.minuto,
+                    relogio.segundo);
+                
                 if (xQueueReceive(adc_queue, &adc_data, 0))
                 {
                     ESP_LOGI(TAG_ADC,
@@ -413,6 +565,101 @@ void timer_task(void *arg)
             }
         }
     }
+}
+
+// ================= TASK DISPLAY ===============
+
+void display_task(void *arg)
+{
+
+    ESP_LOGI(TAG, "Initialize I2C bus");
+    i2c_master_bus_handle_t i2c_bus = NULL;
+    i2c_master_bus_config_t bus_config = {
+        .clk_source = I2C_CLK_SRC_DEFAULT,
+        .glitch_ignore_cnt = 7,
+        .i2c_port = I2C_BUS_PORT,
+        .sda_io_num = EXAMPLE_PIN_NUM_SDA,
+        .scl_io_num = EXAMPLE_PIN_NUM_SCL,
+        .flags.enable_internal_pullup = true,
+    };
+    ESP_ERROR_CHECK(i2c_new_master_bus(&bus_config, &i2c_bus));
+
+    ESP_LOGI(TAG, "Install panel IO");
+    esp_lcd_panel_io_handle_t io_handle = NULL;
+    esp_lcd_panel_io_i2c_config_t io_config = {
+        .dev_addr = EXAMPLE_I2C_HW_ADDR,
+        .scl_speed_hz = EXAMPLE_LCD_PIXEL_CLOCK_HZ,
+        .control_phase_bytes = 1,               // According to SSD1306 datasheet
+        .lcd_cmd_bits = EXAMPLE_LCD_CMD_BITS,   // According to SSD1306 datasheet
+        .lcd_param_bits = EXAMPLE_LCD_CMD_BITS, // According to SSD1306 datasheet
+        .dc_bit_offset = 6,                     // According to SSD1306 datasheet
+
+    };
+    ESP_ERROR_CHECK(esp_lcd_new_panel_io_i2c(i2c_bus, &io_config, &io_handle));
+
+    ESP_LOGI(TAG, "Install SSD1306 panel driver");
+    esp_lcd_panel_handle_t panel_handle = NULL;
+    esp_lcd_panel_dev_config_t panel_config = {
+        .bits_per_pixel = 1,
+        .reset_gpio_num = EXAMPLE_PIN_NUM_RST,
+    };
+
+    esp_lcd_panel_ssd1306_config_t ssd1306_config = {
+        .height = EXAMPLE_LCD_V_RES,
+    };
+    panel_config.vendor_config = &ssd1306_config;
+    
+    ESP_ERROR_CHECK(esp_lcd_new_panel_ssd1306(io_handle, &panel_config, &panel_handle));
+    ESP_ERROR_CHECK(esp_lcd_panel_reset(panel_handle));
+    ESP_ERROR_CHECK(esp_lcd_panel_init(panel_handle));
+    ESP_ERROR_CHECK(esp_lcd_panel_disp_on_off(panel_handle, true));
+
+    ESP_LOGI(TAG, "Initialize LVGL");
+    lv_init();
+    // create a lvgl display
+    lv_display_t *display = lv_display_create(EXAMPLE_LCD_H_RES, EXAMPLE_LCD_V_RES);
+    // associate the i2c panel handle to the display
+    lv_display_set_user_data(display, panel_handle);
+    // create draw buffer
+    void *buf = NULL;
+    ESP_LOGI(TAG, "Allocate separate LVGL draw buffers");
+    // LVGL reserves 2 x 4 bytes in the buffer, as these are assumed to be used as a palette.
+    size_t draw_buffer_sz = EXAMPLE_LCD_H_RES * EXAMPLE_LCD_V_RES / 8 + EXAMPLE_LVGL_PALETTE_SIZE;
+    buf = heap_caps_calloc(1, draw_buffer_sz, MALLOC_CAP_INTERNAL | MALLOC_CAP_8BIT);
+    assert(buf);
+
+    // LVGL9 suooprt new monochromatic format.
+    lv_display_set_color_format(display, LV_COLOR_FORMAT_I1);
+    // initialize LVGL draw buffers
+    lv_display_set_buffers(display, buf, NULL, draw_buffer_sz, LV_DISPLAY_RENDER_MODE_FULL);
+    // set the callback which can copy the rendered image to an area of the display
+    lv_display_set_flush_cb(display, example_lvgl_flush_cb);
+
+    ESP_LOGI(TAG, "Register io panel event callback for LVGL flush ready notification");
+    const esp_lcd_panel_io_callbacks_t cbs = {
+        .on_color_trans_done = example_notify_lvgl_flush_ready,
+    };
+    /* Register done callback */
+    esp_lcd_panel_io_register_event_callbacks(io_handle, &cbs, display);
+
+    ESP_LOGI(TAG, "Use esp_timer as LVGL tick timer");
+    const esp_timer_create_args_t lvgl_tick_timer_args = {
+        .callback = &example_increase_lvgl_tick,
+        .name = "lvgl_tick"
+    };
+    esp_timer_handle_t lvgl_tick_timer = NULL;
+    ESP_ERROR_CHECK(esp_timer_create(&lvgl_tick_timer_args, &lvgl_tick_timer));
+    ESP_ERROR_CHECK(esp_timer_start_periodic(lvgl_tick_timer, EXAMPLE_LVGL_TICK_PERIOD_MS * 1000));
+
+    ESP_LOGI(TAG, "Create LVGL task");
+    xTaskCreate(example_lvgl_port_task, "LVGL", EXAMPLE_LVGL_TASK_STACK_SIZE, NULL, EXAMPLE_LVGL_TASK_PRIORITY, NULL);
+
+    ESP_LOGI(TAG, "Display LVGL Scroll Text");
+    // Lock the mutex due to the LVGL APIs are not thread-safe
+    _lock_acquire(&lvgl_api_lock);
+    example_lvgl_demo_ui(display);
+    _lock_release(&lvgl_api_lock);
+
 }
 
 // ================= MAIN =================
@@ -473,6 +720,7 @@ void app_main(void)
     xTaskCreate(timer_task, "timer", 4096, NULL, 5, NULL);
     xTaskCreate(pwm_task, "pwm", 4096, NULL, 6, NULL);
     xTaskCreate(adc_task, "adc", 4096, NULL, 6, NULL);
+    xTaskCreate(display_task, "display", 4096, NULL, 4, NULL);
 
     //ISR
 
